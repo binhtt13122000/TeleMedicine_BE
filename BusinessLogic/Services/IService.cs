ddc1dbbbe8c339ef99ef3b9b7095de37669f2855
binhtt131222000 <binhtt.13122000@gmail.com>
2021-09-24 15:51:54 +0700

    BINHTT: Create IService

---
+++ BusinessLogic/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public interface IService<T, TKey>
+    {
+        IQueryable<T> GetAll(params Expression<Func<T, object>>[] includes);
+        Task<T> AddAsync(T entity);
+        Task<T> GetByIdAsync(TKey id);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+    }
+}
