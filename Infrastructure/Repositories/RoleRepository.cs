46e0ea15a374ba2aa3e8d55c1a90ce4e38018392
binhtt131222000 <binhtt.13122000@gmail.com>
2021-09-24 13:55:07 +0700

    create repository for role

---
+++ Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Interfaces.Implements;
+using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public interface IRoleRepository : IRepository<Role, int>
+    {
+    }
+    public class RoleRepository: Repository<Role, int>, IRoleRepository
+    {
+        public RoleRepository(TeleMedicineContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
