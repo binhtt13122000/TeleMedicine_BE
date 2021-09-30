c6f5298365febaf049a4e12118bffe92c963667b
DanhVanPham <danhskipper18@gmail.com>
2021-09-30 09:51:54 +0700

    [DanhPV] add authorize

--- TeleMedicine_BE/Controllers/SlotController.cs
+++ TeleMedicine_BE/Controllers/SlotController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BusinessLogic.Services;
 using Infrastructure.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,7 @@ namespace TeleMedicine_BE.Controllers
 {
     [Route("api/v1/slots")]
     [ApiController]
+    [Authorize]
     public class SlotController : Controller
     {
         private readonly ISlotService _slotService;
