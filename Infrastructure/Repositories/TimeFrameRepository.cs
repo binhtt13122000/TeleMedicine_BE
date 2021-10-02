904833f8a740ebf141176e82c65dff862e782fed
DanhVanPham <danhskipper18@gmail.com>
2021-10-02 10:53:31 +0700

    [DanhPV] create time frame with list

--- Infrastructure/Repositories/TimeFrameRepository.cs
+++ Infrastructure/Repositories/TimeFrameRepository.cs
@@ -11,6 +11,7 @@ namespace Infrastructure.Repositories
 {
     public interface ITimeFrameRepository : IRepository<TimeFrame, int>
     {
+        Task<bool> AddTimeFramesAsync(List<TimeFrame> timeFrames);
     }
     public class TimeFrameRepository : Repository<TimeFrame, int>, ITimeFrameRepository
     {
@@ -19,5 +20,25 @@ namespace Infrastructure.Repositories
 
         }
 
+        public async Task<bool> AddTimeFramesAsync(List<TimeFrame> timeFrames)
+        {
+            if (timeFrames == null)
+            {
+                throw new ArgumentNullException($"{nameof(AddTimeFramesAsync)} entity must not be null");
+            }
+            var transaction = _dbContext.Database.BeginTransaction();
+            try
+            {
+                await _dbContext.AddRangeAsync(timeFrames);
+                int updatedEntry = await _dbContext.SaveChangesAsync();
+                transaction.Commit();
+                return updatedEntry > 0;
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw new Exception($"{nameof(timeFrames)} could not be saved: {ex.Message}");
+            }
+        }
     }
 }
